579491a6092b1e8327ea7a3981bad90b3a1e1f5f
Ali Bakhtiari <ali.bakhtiari@7-11.com>
2019-02-19 08:18:53 -0600

    first crack at the problem

---
+++ dataloader.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+
+def preprocess():
+    '''
+    implements following preprocessing steps
+    '''
+    pass
+
+def load_data():
+    '''
+    loads data
+    '''
+    pass
+
+def data_generator():
+    '''
+    generator that loads data in batch
+    '''
+    pass
